f92d0386ae517cd69baf272be4def89c65c6cbe7
chtrli <chtrl2022@gmail.com>
2022-06-30 21:34:16 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app1';
+  password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  length = 0;
+
+  onChangeUseLetters(){
+   this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(value: string){
+   this.length = parseInt(value);
+   console.log(this.length);
+  }
+
+  onButtonClick(){
+  const numbers = '1234567890';
+  const letters = 'abcdefghijklmnopqrstuvwxyz';
+  const symbols = '!@#$%^&*()';
+  let validChars = '';
+
+  if(this.includeLetters){
+    validChars = validChars + letters;
+  }
+  if(this.includeNumbers){
+    validChars = validChars + numbers;
+  }
+  if(this.includeSymbols){
+    validChars = validChars + symbols;
+  }
+
+  let generatedPassword = '';
+  for(let i =0;i<=this.length;i++){
+    const index = Math.floor(Math.random() * validChars.length);
+    generatedPassword = generatedPassword + validChars[index];
+  }
+
+  this.password = generatedPassword;
+  }
 }
